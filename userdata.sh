1918b787ecf9b11fbbb21066a900b0cf31110540
ravishankar920 <43026201+ravishankar920@users.noreply.github.com>
2022-11-17 10:45:44 +0530

    userdata for docker instance creation

---
+++ userdata.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+sudo yum update -y
+
+sudo yum install -y docker
+
+sudo service docker start
+
+sudo usermod -a -G docker ec2-user
+
+sudo amazon-linux-extras install java-openjdk11 -y
